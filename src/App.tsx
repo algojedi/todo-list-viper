85f39a3f27d72a601bb190dfd8c85d91d3bf74c1
alogjedi <vicbanwait@hotmail.com>
2021-12-25 21:37:18 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import './App.css';
 import { Header } from './components/header/Header';
 import { InputTodo } from './components/input-todo/InputTodo';
 import { TodoList } from './components/todos-list/TodoList';
-import { TodosProvider } from './contexts/TodoContext';
 
 function App() {
+  
   return (
     <div className="App">
       <Header />
-      <TodosProvider>
-        <InputTodo />
-        <TodoList />
-      </TodosProvider>
+      <InputTodo />
+      <TodoList />
     </div>
   );
 }
